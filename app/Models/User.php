49b651ee70c6ee31bca15b18a73be2d751bc1c0b
DavlatTredo <davlatbek@tredo.co>
2022-10-16 20:41:42 +0600

    added model load for permission hobbies

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable implements JWTSubject
     use HasRoles;
     use Notifiable;
 
+    protected $with = ['hobbies'];
     /**
      * The attributes that are mass assignable.
      *
