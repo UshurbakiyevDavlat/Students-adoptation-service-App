a1928554dc9882443dd1529f6004c90e7b74e742
DavlatTredo <davlatbek@tredo.co>
2022-10-16 17:34:09 +0600

    reset routes for profile

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ Route::group([
     'prefix' => 'auth'
 ], static function () {
     Route::post('login', [AuthController::class,'login']);
-    Route::get('logout',  [AuthController::class,'logout']);
-    Route::get('refresh',  [AuthController::class,'refresh']);
-    Route::get('me',  [AuthController::class,'me']);
+    Route::post('logout',  [AuthController::class,'logout']);
+    Route::post('refresh',  [AuthController::class,'refresh']);
+    Route::post('me',  [AuthController::class,'me']);
 });
 
 Route::prefix('user')->group(
