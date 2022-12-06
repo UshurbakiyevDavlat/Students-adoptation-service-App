2341f4531893861aab61e1886b8c43ec6e802172
Давлат Ушурбакиев <davlatbek@tredo.co>
2022-12-06 12:13:50 +0600

    added swagger, added welcome page to it, refactored some code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+* @OA\Info(title="UnikDiploma API", version="0.1")
+* @OA\Get(
+*     path="/",
+*     description="Home page",
+*     @OA\Response(response="default", description="Welcome page")
+* )
+*/
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
 }
