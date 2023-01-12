d14693d50808ba0a7044d83908af7ef472c8e4ae
Давлат Ушурбакиев <davlatbek@tredo.co>
2023-01-12 15:46:01 +0600

    added pagination for users collection and refactored elastic and local controllers directories

--- app/Http/Controllers/Services/ElasticSearchController.php
+++ app/Http/Controllers/Services/ElasticSearchController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Services;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Elastic\Elasticsearch\Client;
 use Elastic\Elasticsearch\ClientBuilder;
