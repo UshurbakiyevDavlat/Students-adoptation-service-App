3a13d44f48e1816d524e17a2203f04c2be6f8ca3
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-05-10 20:31:16 +0600

    updated middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,15 +6,11 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if ($request->wantsJson()) {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
     }
+
 }
