782145531c5880573746639c7325b3c00c4c07b6
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-05-15 23:04:18 +0600

    debug

--- app/Observers/MessageObserver.php
+++ app/Observers/MessageObserver.php
@@ -4,12 +4,14 @@ namespace App\Observers;
 
 use App\Events\MessagePushNotification;
 use App\Models\Messages;
+use Illuminate\Support\Facades\Log;
 
 class MessageObserver
 {
     public function created(Messages $message): void
     {
         $user = auth()->user();
+        Log::info('Message' . $message->text);
         event(new MessagePushNotification($message, $user));
     }
 }
