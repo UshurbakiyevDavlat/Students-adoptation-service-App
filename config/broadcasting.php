a1fcdeb06be355b8d1a44792d52b5d33750e71be
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-06-04 12:56:44 +0600

    updated

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,6 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => false,
+                'host' => env('APP_URL'),
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
