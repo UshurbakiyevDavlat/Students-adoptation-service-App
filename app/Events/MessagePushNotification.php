6f61ae9f8be2f23a7aacbf55f330eee8ecef8fd9
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-06-04 12:45:49 +0600

    debug

--- app/Events/MessagePushNotification.php
+++ app/Events/MessagePushNotification.php
@@ -32,13 +32,13 @@ class MessagePushNotification implements ShouldBroadcast
         $this->user = $user;
     }
 
-    /**
-     * @throws \JsonException
-     */
-    public function broadcastWith(): bool|string
+    public function broadcastWith(): array
     {
         $data = (new WebsocketResource($this->message))->toArray(request());
-        return json_encode($data, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE);
+        $encoded = json_encode($data, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE);
+        return [
+            $encoded,
+        ];
     }
 
     /**
