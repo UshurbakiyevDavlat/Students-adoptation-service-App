842cdf96d13eeae5046eb742cb0a9f462d224c67
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-06-05 22:21:36 +0600

    fix

--- app/Http/Resources/Friend/FriendCollection.php
+++ app/Http/Resources/Friend/FriendCollection.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Friend;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Storage;
 
 class FriendCollection extends ResourceCollection
 {
@@ -14,6 +15,8 @@ class FriendCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $data =  parent::toArray($request);
+        $data['avatar'] = Storage::url($data['avatar']);
+        return $data;
     }
 }
